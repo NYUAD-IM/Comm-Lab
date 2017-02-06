1d4e40e9b28a106cfbaf31f3f84c1b541b7a7c9f
Pierre Depaz <pierre.depaz@gmail.com>
2017-02-06 17:05:50 +0400

    added DOM pure JS elements

---
+++ 4a_DOM_Javascript/script.js
@@ -0,0 +1,19 @@
+console.log('hello!');
+
+//first we need to find our div in the DOM
+//we do that through the unique id we gave it
+//and then we store it in a variable called button_darker
+var button = document.getElementById("theButton");
+
+//then we add an EventListener to our button
+//this means we need to specify which event we're listening for (in our case, a click event)
+//and, in the second part, the action we want to take place when that happens
+//it is going to be a function which hides the related div
+button.addEventListener('click', function(){
+
+  //similar to line 6, we find the element we want to affect
+  var square = document.getElementById("theSquare");
+
+  //we then set its opacity to none, by selecting it through its 'style' attribute (i.e. accessing its CSS)
+  square.style.visibility = 'hidden';
+});
---
+++ 4b_DOM_Javascript_2/script.js
@@ -0,0 +1,76 @@
+console.log('hello!');
+
+//first we need to find our div in the DOM
+//we do that through the unique id we gave it
+//and then we store it in a variable called button_darker
+var button_darker = document.getElementById("darker");
+
+//then we add an EventListener to our button
+//this means we need to specify which event we're listening for (in our case, a click event)
+//and, in the second part, the action we want to take place when that happens
+//it is going to be a function which hides the related div
+button_darker.addEventListener('click', function(){
+
+  //similar to line 6, we find the element we want to affect
+  var square_darker = document.getElementById("square_darker");
+
+  //now that we have the element we want to keep, we need to find the elements we want to have disappear
+  var other_squares = document.getElementsByClassName("squares");
+
+  //then, we use a for loop to go through all of these other elements
+  for(var i = 0; i < other_squares.length; i++){
+    other_squares[i].style.visibility = 'hidden';
+  }
+
+  //so now that we got rid of all the squares, we need to force our specific square to re-appear.
+  square_darker.style.visibility = 'visible';
+});
+
+//and then we do it again for all the other buttons!
+
+//for the dark button
+var button_dark = document.getElementById("dark");
+button_dark.addEventListener('click', function(){
+
+  var square_dark = document.getElementById("square_dark");
+
+  var other_squares = document.getElementsByClassName("squares");
+
+  for(var i = 0; i < other_squares.length; i++){
+    other_squares[i].style.visibility = 'hidden';
+  }
+
+  square_dark.style.visibility = 'visible';
+});
+
+
+//for the light button
+var button_light = document.getElementById("light");
+button_light.addEventListener('click', function(){
+
+  var square_light = document.getElementById("square_light");
+
+  var other_squares = document.getElementsByClassName("squares");
+
+  for(var i = 0; i < other_squares.length; i++){
+    other_squares[i].style.visibility = 'hidden';
+  }
+
+  square_light.style.visibility = 'visible';
+});
+
+
+//for the lighter button
+var button_lighter = document.getElementById("lighter");
+button_lighter.addEventListener('click', function(){
+
+  var square_lighter = document.getElementById("square_lighter");
+
+  var other_squares = document.getElementsByClassName("squares");
+
+  for(var i = 0; i < other_squares.length; i++){
+    other_squares[i].style.visibility = 'hidden';
+  }
+
+  square_lighter.style.visibility = 'visible';
+});
