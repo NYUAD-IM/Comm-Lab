eb3fab3fe052f9f4078c9a3601d4622b4572595e
Pierre Depaz <pierre.depaz@gmail.com>
2017-03-29 17:28:28 +0400

    added more in-class examples

---
+++ In_Class_Examples_Pierre/Week_09/svg_handler.js
@@ -0,0 +1,42 @@
+var canvas;
+
+var myImage;
+var rect;
+var circle_red;
+var circle_yellow;
+
+canvas = document.getElementById('myCanvas');
+
+canvas.style.position = 'absolute';
+canvas.style.width = '100%';
+canvas.style.height = '100%';
+
+paper.setup(canvas);
+
+paper.project.importSVG('abstract.svg', function(item, origin){
+  myImage = item;
+
+  rect = myImage.children.rect;
+  circle_red = myImage.children.circles.children.red;
+  circle_yellow = myImage.children.circles.children.yellow;
+
+  rect.onMouseDown = function(){
+    console.log('clicked rectangle');
+
+    rect.style.fillColor = '#349432';
+  };
+
+  circle_red.onMouseDown = function(){
+    console.log('clicked red circle!');
+
+    circle_red.opacity = 0;
+  };
+
+  circle_yellow.onMouseDown = function(){
+    console.log('clicked yellow circle:', circle_yellow.shadowOffset);
+
+    circle_yellow.shadowColor = 'black';
+    circle_yellow.shadowOffset = new paper.Point(5, 5);
+    circle_yellow.shadowBlur = 12;
+  };
+});
