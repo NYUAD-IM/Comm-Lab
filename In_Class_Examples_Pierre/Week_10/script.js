916728a062163e95cefde687c9952823c4f059eb
Pierre Depaz <pierre.depaz@gmail.com>
2017-04-11 13:52:51 +0400

    added in class example pierre

---
+++ In_Class_Examples_Pierre/Week_10/script.js
@@ -0,0 +1,16 @@
+window.addEventListener('load', function(){
+  var theVideo = document.getElementById('vid');
+
+  // theVideo.play();
+
+  theVideo.ontimeupdate = function(){
+    console.log('current time is: '+theVideo.currentTime);
+
+    if(theVideo.currentTime > 7){
+      theVideo.currentTime = 4.8;
+      theVideo.play();
+    }
+
+  };
+
+});
