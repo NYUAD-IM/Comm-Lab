4d78745580ddfb6ff769317b15f6a0fe4cca59b5
Pierre Depaz <pierre.depaz@gmail.com>
2017-05-15 11:11:56 +0400

    added p5play code

--- 12_p5play/script.js
+++ 12_p5play/script.js
@@ -1,20 +1,46 @@
 var ball;
+var ball2;
 
 function preload(){
 }
 
 
 function setup(){
- createCanvas(windowWidth, windowHeight);
- ball=createSprite(200, 200, 200, height-200);
-	ball.addAnimation("floating", "sphere0001.png", "sphere0011.png");
+var cnv = createCanvas(windowWidth, windowHeight);
+// cnv.position(0, 0);
+
+ ball = createSprite(200, 200, 200, height-200);
+
+ ball.addAnimation("floating", "sphere0001.png", "sphere0011.png");
+ ball.setCollider("circle", -100, 100, 50);
+
+ ball2 = createSprite(200, 200, 200, height-200);
+
+ ball2.addAnimation("floating", "sphere0001.png", "sphere0011.png");
+ ball2.setCollider("circle", -100, 100, 50);
+
+ //scale affects the size of the collider
+ball.scale = random(0.5, 1);
+//mass determines the force exchange in case of bounce
+ball.mass = ball.scale;
+
+ball2.scale = random(0.5, 1);
+//mass determines the force exchange in case of bounce
+ball2.mass = ball2.scale;
+
+ball.debug = true;
 }
 
 
 function draw(){
   background(255, 255, 255);
 
-  ball.attractionPoint(.2, mouseX, mouseY);
+  ball.attractionPoint(.05, mouseX, mouseY);
+  ball2.attractionPoint(.1, mouseX, mouseY);
+
+
+ball.bounce(ball2);
+
   drawSprites();
 
 }
