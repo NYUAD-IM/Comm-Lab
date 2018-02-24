a38329273e5b8d40a816b6f9e8972c34a274adca
Pierre Depaz <pierre.depaz@gmail.com>
2018-02-24 14:07:32 +0400

    added playback position and onload events

--- 07_IntroToSound/HowlJS/howl-player.js
+++ 07_IntroToSound/HowlJS/howl-player.js
@@ -1,8 +1,22 @@
+let playback_slider //we are going to need a variable to hold the playback slider
+
+// this function will grab our slider from the HTML document and set its `max` attribute
+// to whatever the duration of the audio clip is!
+let init = () => {
+
+  // grab the html element
+  playback_slider = document.getElementById('playback-position-slider')
+
+  // set its max attribute to the duration of the sound,
+  // so that we don't set a playback position that doesn't exist!
+  playback_slider.setAttribute('max', theSound.duration())
+}
 
 // this is how you create a sound with HowlJS
 let theSound = new Howl({
   src: '../technology.mp4', //this is the path to the sound (required)
-  loop: true //and then optionally you can define whether it will loop (by default, false)
+  loop: true, //and then optionally you can define whether it will loop (by default, false)
+  onload: init //we can also have even listeners! this here will call a function once the sound is totally loaded
 })
 
 // this function is called from a <button> on the HTML page and plays the sound
@@ -14,6 +28,11 @@ let playSound = () => {
 // the argument is the value of the slide on the HTML page
 let adjustVolume = (value) => {
   theSound.volume(value)
+}
+
+// this function uses seek() to set the start position at the given value
+let setPosition = (value) => {
+  theSound.seek(value)
 }
 
 // you can also adjust the rate
