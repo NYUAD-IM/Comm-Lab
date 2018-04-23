dce9490aab0df92215cb70f0b82c16ec9e68faf5
pierre <pierre.depaz@gmail.com>
2018-04-23 14:56:14 +0400

    cleaned play example

--- 12_p5play/script.js
+++ 12_p5play/script.js
@@ -1,5 +1,4 @@
 let ball;
-let ball2;
 
 preload = () =>{
 }
@@ -12,21 +11,12 @@ let cnv = createCanvas(windowWidth, windowHeight);
  ball = createSprite(200, 200, 200, height-200);
 
  ball.addAnimation("floating", "sphere0001.png", "sphere0011.png");
- ball.setCollider("circle", -100, 100, 50);
-
- ball2 = createSprite(200, 200, 200, height-200);
-
- ball2.addAnimation("floating", "sphere0001.png", "sphere0011.png");
- ball2.setCollider("circle", -100, 100, 50);
+ ball.setCollider("circle", 0, 0, 200);
 
  //scale affects the size of the collider
 ball.scale = random(0.5, 1);
 //mass determines the force exchange in case of bounce
 ball.mass = ball.scale;
-
-ball2.scale = random(0.5, 1);
-//mass determines the force exchange in case of bounce
-ball2.mass = ball2.scale;
 
 ball.debug = true;
 }
@@ -35,11 +25,7 @@ ball.debug = true;
 draw = () =>{
   background(255, 255, 255);
 
-  ball.attractionPoint(.05, mouseX, mouseY);
-  ball2.attractionPoint(.1, mouseX, mouseY);
-
-
-  ball.bounce(ball2);
+  ball.attractionPoint(.005, mouseX, mouseY);
 
   drawSprites();
 
