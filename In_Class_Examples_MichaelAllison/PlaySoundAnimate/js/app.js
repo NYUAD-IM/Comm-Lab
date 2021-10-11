830c1dd1f898bca213bda6a01fa6ca05e6d0b1df
Mike Allison <philosopher.osopher@gmail.com>
2021-10-11 09:31:27 +0400

    added howler and animate example

---
+++ In_Class_Examples_MichaelAllison/PlaySoundAnimate/js/app.js
@@ -0,0 +1,39 @@
+
+//https://freesound.org/people/moogy73/sounds/425705/
+//converted to 196kbps mp3 with https://online-audio-converter.com/
+
+var woosh = new Howl({
+  src: ['sounds/woosh.mp3'],
+  html5: true
+});
+
+var big = true;
+
+$(".special").click(function(){
+
+  woosh.play();
+  
+  let params;
+
+  if(big){
+    params = {
+      width: "100px",
+      height: "100px",
+      fontSize: "10px",
+      borderWidth: "1px"
+    };
+    big = false;
+  }
+  else {
+    params = {
+      width: "500px",
+      height: "500px",
+      fontSize: "30px",
+      borderWidth: "3px"
+    };
+    big = true;
+  }
+
+  $(".item").animate(params, 500);
+
+});
